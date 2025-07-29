d41246582c851cd7446345c139cb48ac28b54ffa
Wahab Ismail Adewale <wismailadewale@gmail.com>
2025-07-29 19:25:57 +0100

    Fix: import send_from_directory to fix favicon route 500 error

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
