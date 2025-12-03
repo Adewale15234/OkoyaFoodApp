22ae176d1ed6086596c5d6b9e54ccd103e86a66d
Wahab Ismail Adewale <wismailadewale@gmail.com>
2025-12-03 20:21:42 +0100

    Removed passport column from Worker model

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ class Worker(db.Model):
     bank_account = db.Column(db.String(50), nullable=True)
     bank_account_name = db.Column(db.String(100), nullable=False)
     guarantor = db.Column(db.String(100), nullable=False)
-    passport = db.Column(db.String(100))  # Added field for passport filename
+    #passport = db.Column(db.String(100))  # Added field for passport filename
 
     attendance_records = db.relationship(
         'Attendance',
