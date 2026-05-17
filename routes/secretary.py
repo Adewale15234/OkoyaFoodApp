c5dd4b9704d628536350c14f05ddd52e11575a3a
Wahab Ismail Adewale <wismailadewale@gmail.com>
2026-05-17 12:01:03 +0100

    update Okoya System

--- routes/secretary.py
+++ routes/secretary.py
@@ -1,9 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect, url_for, flash
 from utils import login_required
+from models import Worker
 
 secretary_bp = Blueprint('secretary', __name__)
 
 @secretary_bp.route('/secretary_dashboard')
 @login_required(role='secretary')
 def secretary_dashboard():
-    return render_template('secretary_dashboard.html')
+    return render_template(
+        'secretary_dashboard.html',
+        workers=Worker.query.filter_by(is_active=True).all(),
+        total_workers=Worker.query.count(),
+        active_count=Worker.query.filter_by(is_active=True).count(),
+        inactive_count=Worker.query.filter_by(is_active=False).count()
+    )
